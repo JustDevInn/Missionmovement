1b7afa69e5224a8c4305038d5f0a40f483f16f61
Justin Peeters <justinpeeters@MacBook-Pro-van-Justin.local>
2025-04-24 14:43:07 +0800

    added preloader html-css only

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+// 🧹 Remove the preloader once React has mounted
+document.getElementById('preloader')?.remove();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
